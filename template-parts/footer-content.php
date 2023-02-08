e434f57920364b0c8dea3c126c458e8bad8e7f2b
Nikolaos <nickpapadaki@hotmail.com>
2023-02-08 15:04:24 +0100

    changed to more open template

--- template-parts/footer-content.php
+++
@@ -1,5 +0,0 @@
-<footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-499b">
-  <div class="u-clearfix u-sheet u-sheet-1">
-    <p class="u-small-text u-text u-text-variant u-text-1">SATRAI Living Lab Télécom SudParis / IP Paris 2023</p>
-  </div>
-</footer>
